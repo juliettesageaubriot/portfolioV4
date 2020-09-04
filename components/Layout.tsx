c771c9710d961bcc6c166e3c691772aed3e7d444
Juliette <juliette.sageaubriot@gmail.com>
2020-09-04 19:05:54 +0200

    feat: updte footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,12 +17,12 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
 
 
     <header id="mainHeader" className="section section_header" data-template="sectionHeader">
-        <div className="header header_content">
-          <Link href="/">
-            <a className="header__logo">JSA</a>
-          </Link>
-          {/* Menu en portable */}
-          {/* <Menu {...props} right pageWrapId={"page-wrap"} outerContainerId={"App"}>
+      <div className="header header_content">
+        <Link href="/">
+          <a className="header__logo">JSA</a>
+        </Link>
+        {/* Menu en portable */}
+        {/* <Menu {...props} right pageWrapId={"page-wrap"} outerContainerId={"App"}>
             <ul>
               <li>
                 <Link href="/">
@@ -45,35 +45,51 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
             </ul>
           </Menu> */}
 
-          {/* Menu desktop */}
-          <ul className="header__right">
-            <li>
-              <Link href="/">
-                <a className="up hover-font">Contact</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a className="up hover-font">About</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a className="up">FR</a>
-              </Link>
-              <Link href="/" >
-                <a className="up cut">EN</a>
-              </Link>
-            </li>
+        {/* Menu desktop */}
+        <ul className="header__right">
+          <li>
+            <Link href="/">
+              <a className="up hover-font">Contact</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a className="up hover-font">About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a className="up">FR</a>
+            </Link>
+            <Link href="/" >
+              <a className="up cut">EN</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+    {children}
+    <footer id="mainFooter" className="section section_footer" data-template="sectionFooter">
+      <div className="footer footer_content">
+        <div className="footer__mentions">
+          <ul>
+            <li>© Juliette SAGE—AUBRIOT 2020</li>
+            <li>All rights reserved</li>
+            <li><Link href="#"><a className="hover-font">Mentions légales</a></Link></li>
           </ul>
         </div>
-      </header>
+        <div className="footer__medias">
+          <ul>
+            <li><Link href="#"><a className="up hover-font"><i className="fab fa-github"></i></a></Link></li>
+            <li><Link href="#"><a className="up hover-font">2</a></Link></li>
+            <li><Link href="#"><a className="up hover-font">3</a></Link></li>
+            <li><Link href="#"><a className="up hover-font">4</a></Link></li>
+          </ul>
+        </div>
+      </div>
+      <div>
 
-
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
+      </div>
     </footer>
   </div>
 )
